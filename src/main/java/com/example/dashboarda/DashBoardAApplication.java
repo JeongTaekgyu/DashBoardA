01544e91a5317b2ccd4698d542ff92a4b89caba0
taekgyu <taekgyu0602@gmail.com>
2022-11-29 23:09:08 +0900

    first commit

---
+++ src/main/java/com/example/dashboarda/DashBoardAApplication.java
@@ -0,0 +1,13 @@
+package com.example.dashboarda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashBoardAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashBoardAApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dashboarda/DashBoardAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dashboarda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashBoardAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
