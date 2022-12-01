241bdb7c71f8a6f05cb329009077a63b2ed32cb7
taekgyu <taekgyu0602@gmail.com>
2022-12-01 23:26:39 +0900

    user, comment, password가 db 예약어라서 Syntax 에러 난것 같다... 전에는 이러지 않았는데.. 정확히 알자

--- src/main/java/com/example/dashboarda/DashBoardAApplication.java
+++ src/main/java/com/example/dashboarda/DashBoardAApplication.java
@@ -2,7 +2,9 @@ package com.example.dashboarda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // Spring Data JPA에서 시간에 대해서 자동으로 값을 넣어주는 기능이다.
 @SpringBootApplication
 public class DashBoardAApplication {
 
