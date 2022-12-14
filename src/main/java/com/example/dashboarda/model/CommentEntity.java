c90f3f439f85c833fe3186d80312a9605e423f15
JeongTaekgyu <taekgyu0602@gmail.com>
2022-12-14 12:28:12 +0900

    Entity 이름을 변경함

---
+++ src/main/java/com/example/dashboarda/model/CommentEntity.java
@@ -0,0 +1,28 @@
+package com.example.dashboarda.model;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "comments")
+public class CommentEntity extends TimeStamped{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "comments", nullable = false)
+    private String comments;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "users_id", nullable = false)
+    private UserEntity user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private PostEntity post;
+
+}
---
+++ src/main/java/com/example/dashboarda/model/PostLikesEntity.java
@@ -0,0 +1,24 @@
+package com.example.dashboarda.model;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "post_likes")
+public class PostLikesEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY) // ManyToOne의 default는 EAGER,
+    @JoinColumn(name = "users_id", nullable = false)
+    private UserEntity user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private PostEntity post;
+}
